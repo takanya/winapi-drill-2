dfb8daf15206cdba5aa18c0fce515d981f30fbce
nebokenamako <takanya@gmail.com>
2025-11-06 11:10:06 +0900

    初期コミット

---
+++ prj03-zine-dlg/prj03-zine-dlg.h
@@ -0,0 +1,1 @@
+// prj03-zine-dlg.h
---
+++ prj03-zine-dlg/stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	prj03-zine-dlg.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
